7a5d9b00d70d6e323fdb4ae047447294eb1b8b9d
RichardManSheng <ooxfordck@gmail.com>
2022-09-08 17:46:59 +0800

    Create codsign.sh

---
+++ codsign.sh
@@ -0,0 +1,22 @@
+codesign -vvv -d vesc_tool_3.00.app
+
+Executable=../vesc_tool_3.00.app/Contents/MacOS/vesc_tool_3.00
+Identifier=itd.vesc-tool-3.00
+Format=app bundle with Mach-O thin (x86_64)
+CodeDirectory v=20400 size=246262 flags=0x0(none) hashes=7689+3 location=embedded
+Hash type=sha256 size=32
+CandidateCDHash sha256=60edb1e5fb6db2465ac8520334cdba48b02ec8c5
+CandidateCDHashFull sha256=60edb1e5fb6db2465ac8520334cdba48b02ec8c56d6efcfb0db94d2301ed8855
+Hash choices=sha256
+CMSDigest=60edb1e5fb6db2465ac8520334cdba48b02ec8c56d6efcfb0db94d2301ed8855
+CMSDigestType=2
+CDHash=60edb1e5fb6db2465ac8520334cdba48b02ec8c5
+Signature size=8969
+Authority=Developer ID Application: Kai Hauser (S5A82Y4LMM)
+Authority=Developer ID Certification Authority
+Authority=Apple Root CA
+Timestamp=13. Apr 2021 at 22:20:03
+Info.plist entries=9
+TeamIdentifier=S5A82Y4LMM
+Sealed Resources version=2 rules=13 files=46
+Internal requirements count=1 size=180
